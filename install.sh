faa3006d71c72d237283851b4a12639df2660371
mortaromarcello <mortaromarcello@gmail.com>
2013-11-10 09:59:03 +0100

    modificato:   install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 dest="/usr/local/share/mma"
 exe="/usr/local/bin/mma"
-mkdir -p $dest
+mkdir -pv $dest
 cp -v mma.py $exe
 for dir in lib includes MMA docs; do
-  cp -r $dir $dest
+  cp -rv $dir $dest
 done
 $exe -G
 chmod a+w $dest/lib/stdlib/.mmaDB
